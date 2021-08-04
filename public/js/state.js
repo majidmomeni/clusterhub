acf6d2208674a1080e6906a7e42f2eea031dbfb6
Ramtin Soltani <ramtinsoltani93@gmail.com>
2021-08-04 03:49:42 -0700

    added addHub and getHubsList methods to StateManager class; moved data inside the class as a private field; moved generateId function inside the class as a static method.

---
+++ public/js/state.js
@@ -0,0 +1,38 @@
+class StateManager {
+
+  #data = {};
+
+  addHub(name) {
+
+    const id = StateManager.generateId();
+
+    this.#data[id] = {
+      id,
+      name: name.trim(),
+      date: Date.now(),
+      posts: {}
+    };
+
+  }
+
+  getHubsList() {
+
+    return Object.values(this.#data)
+    .sort((a, b) => b.date - a.date)
+    .map(hub => ({ id: hub.id, name: hub.name, date: hub.date }));
+
+  }
+
+  static generateId() {
+
+    const charset = 'qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
+    let id = '';
+
+    for ( let i = 0; i < 10; i++ )
+      id += charset[Math.floor(Math.random() * charset.length)];
+
+    return id;
+
+  }
+
+}
