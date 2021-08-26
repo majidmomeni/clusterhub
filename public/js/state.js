d63a3c91091e6479b06413b41e8b271b90cf8b1f
Majid Momeni <majidmo245@gmail.com>
2021-08-26 13:19:09 +0430

    made header,footer and main

--- public/js/state.js
+++ public/js/state.js
@@ -1,42 +1,40 @@
-
-
-class StateManger {
+class StateManager extends EventTarget {
 
   #data = {};
 
-  addhub(name) {
-    
-    const id = StateManger.generateId();
-    
+  addHub(name) {
+
+    const id = StateManager.generateId();
+
     this.#data[id] = {
       id,
       name: name.trim(),
       date: Date.now(),
       posts: {}
     };
+
+    this.dispatchEvent(new Event('statechange'));
+
   }
 
-  getHubslist() {
+  getHubsList() {
 
     return Object.values(this.#data)
-    .sort((a , b) => b.date - a.date)
-    .map(hub => {
-      ({ id: hub.id, name: hub.name, date: hub.date})
-    });
-  }
+    .sort((a, b) => b.date - a.date)
+    .map(hub => ({ id: hub.id, name: hub.name, date: hub.date }));
 
+  }
 
   static generateId() {
-  
 
-    const charset = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+    const charset = 'qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM';
     let id = '';
-  
-    for (let i = 0; i < 10; i++) 
-      id += charset[Math.floor(Math.random() * charset.length)]
-  
-   return id
+
+    for ( let i = 0; i < 10; i++ )
+      id += charset[Math.floor(Math.random() * charset.length)];
+
+    return id;
+
   }
+
 }
-
-
